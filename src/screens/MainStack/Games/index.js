757a7bf3ebbfcca3416e668b2f5c2e89e973c372
Kamil Rusin <lotrusek@gmail.com>
2020-05-05 22:48:17 +0200

    feat: add displaying games from firebase

--- src/screens/MainStack/Games/index.js
+++ src/screens/MainStack/Games/index.js
@@ -3,41 +3,29 @@ import database from '@react-native-firebase/database';
 import GeneralList from '../_components/GeneralList';
 import { GAME_TYPE } from '../_utils/constants';
 
-//item: {id: string, download_url : string, title: string, publisher: string}
-const gamesList = [
-  {
-    id: '123',
-    download_url:
-      'https://c-sf.smule.com/sf/s26/arr/d2/51/fe94c92f-b7bf-4f29-aee5-5fc99bef9a0f.jpg',
-    title: 'Witcher 3: Wild Hunt',
-    publisher: 'CD Projekt RED'
-  },
-  {
-    id: '123',
-    download_url:
-      'https://s3.amazonaws.com/media.muckrack.com/profile/images/9492864/24608-coffin-dance-viral-.jpg.256x256_q100_crop-smart.jpg',
-    title: 'Coffin 1',
-    publisher: 'Africa'
-  },
-  {
-    id: '123',
-    download_url: '',
-    title: 'Coffin 2',
-    publisher: 'Africa'
-  }
-];
-
 const GamesScreen = () => {
   const [games, setGames] = useState(null);
 
   useEffect(() => {
-    database()
-      .ref(`/games/`)
+    const subscriber = database()
+      .ref(`/games`)
       .on('value', snapshot => {
-        console.log('User data: ', snapshot.val());
+        let tempGames = [];
+        console.log('Games data: ', snapshot.val());
+
+        snapshot.forEach(childSnapshot => {
+          let item = childSnapshot.val();
+          item.key = childSnapshot.key;
+
+          tempGames.push(item);
+        });
+
+        setGames(tempGames);
+        console.log('Games data v2: ', tempGames);
       });
+    return () => subscriber();
   }, []);
-  return <GeneralList type={GAME_TYPE} data={gamesList} />;
+  return <GeneralList type={GAME_TYPE} data={games} />;
 };
 
 export default GamesScreen;
