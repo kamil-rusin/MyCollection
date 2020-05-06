1cb781bab0302e57f606cf76f78c2570bca2f414
Kamil Rusin <lotrusek@gmail.com>
2020-05-06 12:55:17 +0200

    feat: apply proper colors

--- src/screens/MainStack/_components/GeneralList.js
+++ src/screens/MainStack/_components/GeneralList.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { StyleSheet, ActivityIndicator, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ItemsList from './ItemsList';
+import { returnProperColor } from '../_utils/checkTypes';
 
 const GeneralList = props => {
   return (
     <SafeAreaView style={styles.container}>
       {props.isLoading ? (
         <View style={styles.indicatorContainer}>
-          <ActivityIndicator size={'large'} color={'#007AFF'} />
+          <ActivityIndicator
+            size={'large'}
+            color={returnProperColor(props.type)}
+          />
         </View>
       ) : (
         <ItemsList data={props.data} type={props.type} />
