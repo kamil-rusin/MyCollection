40d0f3ba2e96030a15e2ddf4d0dec08965690241
Kamil Rusin <lotrusek@gmail.com>
2020-05-09 18:54:13 +0200

    feat: add splash screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { createStackNavigator } from '@react-navigation/stack';
+import SplashScreen from 'react-native-splash-screen';
 import configureStore from './redux/Store';
 import AuthorizationScreen from './screens/AuthStack';
 import { NavigationContainer } from '@react-navigation/native';
@@ -13,6 +14,10 @@ const Stack = createStackNavigator();
 
 const App: () => React$Node = () => {
   const isLoggedIn = false;
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <Provider store={store}>
